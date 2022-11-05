1f60ca41ad53ffa00e5d02147b1ef38473f3007a
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-11-05 20:23:24 +0530

    Initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./App.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
